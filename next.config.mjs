bdd5a0b4000977e092793ab96d0402c1f294cfb6
Deel <mac@PROs-MacBook-Pro.local>
2026-07-23 00:49:43 +0700

    fix: remove root to home redirect in next config

--- next.config.mjs
+++ next.config.mjs
@@ -13,15 +13,6 @@ const nextConfig = {
   images: {
     remotePatterns: imageHosts,
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
